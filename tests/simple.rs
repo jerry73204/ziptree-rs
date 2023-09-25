be6c1c48875bf479dfa3c76f54afa9427824e532
howsiyu <howseeu@gmail.com>
2023-09-25 18:45:25 +0100

    Rewrite to use only safe Rust. Use Zip-zip Trees's rank distribution.

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,42 +1,42 @@
+use std::collections::BTreeMap;
 use std::fmt::Debug;
-use std::collections::{HashMap, BTreeMap};
+
 use rand::Rng;
 
 #[test]
 fn simple_test() -> Result<(), Box<dyn Debug>> {
     let mut rng = rand::thread_rng();
-    let mut tree = ziptree::ZipTree::<usize, usize>::new();
+    let mut tree = ziptree::ZipTree::new();
     let mut pairs = BTreeMap::new();
 
-    for _ in 0..(1<<13) {
-
+    for _ in 0..1_000_000 {
         if rng.gen_bool(0.5) {
-            let key = rng.gen();
-            let val = rng.gen();
-            tree.insert(key, val);
-            pairs.insert(key, val);
-            // println!("insert {} {}", key, val);
-        }
-        else {
+            let key = rng.gen::<u16>();
+            let val = rng.gen::<usize>();
+            let expect_old_val: Option<usize> = pairs.insert(key, val);
+            let old_val = tree.insert(key, val);
+
+            // println!("insert {:?} {:?}, {:?} ==? {:?}", key, val, expect_old_val, old_val);
+            assert!(old_val == expect_old_val);
+        } else {
             let key = rng.gen();
             let expect_val = pairs.remove(&key);
             let val = tree.remove(&key);
 
-            // println!("remove {}, {:?} ==? {:?}", key, expect_val, val);
+            // println!("remove {:?}, {:?} ==? {:?}", key, expect_val, val);
             assert!(val == expect_val);
         }
 
-        // tree.dump();
-        for ((key, val), (expect_key, expect_val)) in tree.iter().zip(pairs.iter()) {
-            assert!(key == expect_key && val == expect_val);
-        }
+        // for ((key, val), (expect_key, expect_val)) in tree.iter().zip(pairs.iter()) {
+        //     assert!(key == expect_key && val == expect_val);
+        // }
         assert!(tree.len() == pairs.len());
     }
 
+    // println!("{:?}", tree);
     for ((key, val), (expect_key, expect_val)) in tree.into_iter().zip(pairs.into_iter()) {
         assert!(key == expect_key && val == expect_val);
     }
-    // tree.dump();
 
     Ok(())
 }
